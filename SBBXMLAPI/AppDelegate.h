8002b4821e6d78efe12d7224f2a56dbbf480d6ad
Alain <alain@Alains-MacBook-Pro-4.local>
2013-06-10 19:59:17 +0200

    Initial Commit

---
+++ SBBXMLAPI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SBBXMLAPI
+//
+//  Created by Alain on 10.06.13.
+//  Copyright (c) 2013 Zone Zero Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
